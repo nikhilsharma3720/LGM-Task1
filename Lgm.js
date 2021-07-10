081777c6c1bb9719e4434491d98b08adfb5b5477
nikhilsharma3720 <63100901+nikhilsharma3720@users.noreply.github.com>
2021-07-11 01:54:41 +0530

    Add files via upload

---
+++ Lgm.js
@@ -0,0 +1,45 @@
+ 
+         
+
+ var slideIndex = 1;
+ showSlides(slideIndex);
+ 
+ function plusSlides(n) {
+   showSlides(slideIndex += n);
+ }
+ 
+ function currentSlide(n) {
+   showSlides(slideIndex = n);
+ }
+ 
+ function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("mySlides");
+   var dots = document.getElementsByClassName("demo");
+   var captionText = document.getElementById("caption");
+   if (n > slides.length) {slideIndex = 1}
+   if (n < 1) {slideIndex = slides.length}
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+       dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex-1].style.display = "block";
+   dots[slideIndex-1].className += " active";
+   captionText.innerHTML = dots[slideIndex-1].alt;
+ }
+ 
+ 
+        function initMap() {
+            var test = { lat: -25.363, lng: 131.044 };
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 4,
+                center: test
+            });
+            var marker = new google.maps.Marker({
+                position: test,
+                map: map
+            });
+        }
+ 
